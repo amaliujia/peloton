7fe6f69f09c6cc0a19de77b764dfd52c48ecfe7d
Rui Wang <amaliujia@users.noreply.github.com>
2016-02-08 17:38:46 -0500

    update includes in hash_join_executor

--- src/backend/executor/hash_join_executor.cpp
+++ src/backend/executor/hash_join_executor.cpp
@@ -11,6 +11,7 @@
 //===----------------------------------------------------------------------===//
 
 #include <vector>
+#include <unordered_map>
 
 #include "backend/common/types.h"
 #include "backend/common/logger.h"
