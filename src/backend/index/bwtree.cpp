2b679d3785c016f279eaa8d527283ebb744805ff
tailofjune <tailofjune@gmail.com>
2016-02-19 01:08:40 -0500

    minor change

--- src/backend/index/bwtree.cpp
+++ src/backend/index/bwtree.cpp
@@ -10,14 +10,3 @@
 //
 //===----------------------------------------------------------------------===//
 
-// !!!
-#include "backend/index/bwtree.h"
-#include "backend/index/index_key.h"
-#include "backend/index/pid_table.h"
-#include "backend/storage/tuple.h"
-
-namespace peloton {
-namespace index {
-  PIDTable PIDTable::global_table_;
-}  // End index namespace
-}  // End peloton namespace
