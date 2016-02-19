eef8a7906fc79465bec8cf9ae02e1eb3e0351f7d
amaliujia <amaliujia@163.com>
2016-02-19 15:24:32 -0500

    Add scankey skeleton

--- src/backend/index/bwtree.h
+++ src/backend/index/bwtree.h
@@ -82,6 +82,10 @@ class BWLeafNode : public BWNormalNode<KeyType> {
     NodeType GetType() {
       return NLeaf;
     }
+
+    void ScanKey(__attribute__((unused)) const KeyType& key, __attribute__((unused)) std::vector<ValueType>& ret) {
+
+    }
 };
 
 // template<typename KeyType>
@@ -121,6 +125,10 @@ class BWSplitNode : public BWDeltaNode {
 
     NodeType GetType() {
       return NSplit;
+    }
+
+    KeyType& GetSplitKey() const {
+        return key;
     }
 };
 
@@ -169,7 +177,7 @@ class BWTree {
   KeyEqualityChecker keyEqualityChecker;
 
   private:
-  bool IfRootNode(  __attribute__((unused)) NodeType type){
+  bool IfLeafNode(  __attribute__((unused)) NodeType type){
     return false;
   }
 
@@ -203,74 +211,130 @@ class BWTree {
     return false;
   }
 
-  public :
+    bool InsertEntryUtil(KeyType key, ValueType value, std::vector<PID>& path, PID cur) {
+        // TODO: should read ptr from pid table.
+        BWNode *node_ptr = NULL;
+
+        if (IfLeafNode(node_ptr->GetType())) {
+            while (true) {
+                // TODO: always get node_ptr of PID cur from pid table here;
+                node_ptr = NULL;
+                // If current is split node
+                if (node_ptr->GetType() == NSplit) {
+                    BWSplitNode<KeyType> *split_ptr = static_cast<BWSplitNode<KeyType> *>(node_ptr);
+                    InsertSplitEntry(path[path.size() - 1], split_ptr->key);
+                }
+
+                // If delta chain is too long
+                if (IfChainFul(0)) {
+                    BWNode *new_ptr = NULL;
+                    bool ret = Consolidate(cur, node_ptr);
+
+                    if(ret == false) {
+                        // No matter if succeed, continue;
+                    }
+                    continue;
+                }
+
+                bool if_split = false;
+                // TODO: should get size from node_ptr
+                while (IfOverflow(0)) {
+                    KeyType split_key;
+                    bool ret = Split(cur, node_ptr, split_key);
+                    if_split = (ret == true || if_split == true) ? true : false;
+                    if (ret == false) {
+                        continue;
+                    } else {
+                        // TODO: should keep spliting
+                        InsertSplitEntry(path[path.size() - 1], split_key);
+                        // TODO: update node_ptr to the current split level.
+                        node_ptr = NULL;
+                    }
+                }
+
+                if (if_split == true) {
+                    continue;
+                }
+
+                if (!Duplicate && DupExist(node_ptr, key)) {
+                    return false;
+                }
+
+                if (!DeltaInsert(cur, node_ptr, key)) {
+                    continue;
+                }
+
+                break;
+            }
+
+            return true;
+        } else {
+            PID next_pid = 0;
+            // PID next_pid = node_ptr->GetNextPID(key);
+            path.push_back(cur);
+            return InsertEntryUtil(key, value, path, next_pid);
+        }
+    }
+
+    void ScanKeyUtil(PID cur, KeyType key, std::vector<ValueType>& ret) {
+        //TODO: get node_ptr of cur.
+        BWNode *node_ptr = NULL;
+
+        if (IfLeafNode(node_ptr->GetType())) {
+            unsigned short if_delete = 0;
+
+            while (true) {
+                // TODO: assume no renmoe node delta and merge node delta.
+                // Only handle split node, insert node, delete node.
+                if (node_ptr->GetType() == NSplit) {
+                    BWSplitNode <KeyType> *split_ptr = static_cast<BWSplitNode <KeyType> *>(node_ptr);
+                    KeyType split_key = split_ptr->GetSplitKey();
+                    if (comparator(key, split_key) == true) { // == true means key < split_key
+                        node_ptr = split_key->next;
+                    }
+                } else if(node_ptr->GetType() == NInsert) {
+                    BWInsertNode<KeyType, ValueType> *insert_ptr = static_cast<BWInsertNode<KeyType, ValueType> *>(node_ptr);
+                    if (insert_ptr->key == key && if_delete == 0) {
+                        ret.push_back(insert_ptr->value);
+                        if (!Duplicate) {
+                            break;
+                        }
+                    } else if (insert_ptr->key == key && if_delete > 0) {
+                        if_delete--;
+                    }
+
+                    node_ptr = insert_ptr->next;
+                } else if(node_ptr->GetType() == NDelete) {
+                    BWDeleteNode<KeyType> *delete_ptr = static_cast<BWDeleteNode<KeyType> *>(node_ptr);
+                    if (delete_ptr->key != key ) {
+                        if_delete++;
+                        if (!Duplicate) {
+                            break;
+                        }
+                    } else if (delete_ptr->key == key) {
+                        if_delete++;
+                    }
+
+                    node_ptr = delete_ptr->next;
+                } else if (node_ptr->GetType() == NLeaf) {
+                    BWLeafNode<KeyType, ValueType> *leaf_ptr = static_cast<BWLeafNode<KeyType, ValueType> *>(node_ptr);
+                    leaf_ptr->ScanKey(key, ret);
+                    break;
+                } else { // TODO: handle other cases here.
+                    break;
+                }
+            }
+        } else {
+            PID next_pid = 0;
+            // PID next_pid = node_ptr->GetNextPID(key);
+            ScanKeyUtil(next_pid, key, ret);
+        }
+
+    }
+
+public :
   BWTree(IndexMetadata * indexMetadata): comparator(indexMetadata), keyEqualityChecker(indexMetadata)	{
 
-  }
-
-  bool InsertEntryUtil(KeyType key, ValueType value, std::vector<PID>& path, PID cur) {
-    // TODO: should read ptr from pid table.
-    BWNode *node_ptr = NULL;
-
-    if (IfRootNode(node_ptr->GetType())) {
-      while (true) {
-        // TODO: always get node_ptr of PID cur from pid table here;
-        node_ptr = NULL;
-        // If current is split node
-        if (node_ptr->GetType() == NSplit) {
-          BWSplitNode<KeyType> *split_ptr = static_cast<BWSplitNode<KeyType> *>(node_ptr);
-          InsertSplitEntry(path[path.size() - 1], split_ptr->key);
-        }
-
-        // If delta chain is too long
-        if (IfChainFul(0)) {
-          BWNode *new_ptr = NULL;
-          bool ret = Consolidate(cur, node_ptr);
-
-          if(ret == false) {
-            // No matter if succeed, continue;
-          }
-          continue;
-        }
-
-        bool if_split = false;
-        // TODO: should get size from node_ptr
-        while (IfOverflow(0)) {
-          KeyType split_key;
-          bool ret = Split(cur, node_ptr, split_key);
-          if_split = (ret == true || if_split == true) ? true : false;
-          if (ret == false) {
-            continue;
-          } else {
-            // TODO: should keep spliting
-            InsertSplitEntry(path[path.size() - 1], split_key);
-            // TODO: update node_ptr to the current split level.
-            node_ptr = NULL;
-          }
-        }
-
-        if (if_split == true) {
-          continue;
-        }
-
-        if (!Duplicate && DupExist(node_ptr, key)) {
-          return false;
-        }
-
-        if (!DeltaInsert(cur, node_ptr, key)) {
-          continue;
-        }
-
-        break;
-      }
-
-      return true;
-    } else {
-      PID next_pid = 0;
-      // PID next_pid = node_ptr->GetNextPID(key);
-      path.push_back(cur);
-      return InsertEntryUtil(key, value, path, next_pid);
-    }
   }
 
   bool InsertEntry(KeyType key, ValueType value) {
@@ -279,6 +343,9 @@ class BWTree {
     return ret;
   }
 
+  void ScanKey(KeyType key, std::vector<ValueType>& ret) {
+      ScanKeyUtil(ROOT_PID, key, ret);
+  }
 };
 
 }  // End index namespace
