6bf8fd3b7bd6176c45e307de62276a9a32982f99
tailofjune <tailofjune@gmail.com>
2016-02-28 01:43:15 -0500

    reformat

--- src/backend/index/bwtree.h
+++ src/backend/index/bwtree.h
@@ -11,21 +11,17 @@
 //===----------------------------------------------------------------------===//
 
 #pragma once
-//
+
 #include "backend/catalog/manager.h"
 #include "backend/common/platform.h"
 #include "backend/common/types.h"
 #include "backend/index/index.h"
-//#include "backend/index/pid_table.h"
 #include "backend/common/value.h"
 #include "backend/index/item_pointer_comparator.h"
 #include "backend/index/item_pointer_equality_checker.h"
 
 
 #include <boost/lockfree/stack.hpp>
-
-
-
 #include <cstdint>
 #include <vector>
 #include <map>
@@ -34,13 +30,7 @@
 
 namespace peloton {
   namespace index {
-
-
     typedef std::uint_fast32_t PID;
-
-
-
-
     typedef size_t size_type;
     typedef uint_fast8_t VersionNumber;
     constexpr int max_chain_len = 8;
@@ -51,13 +41,13 @@ namespace peloton {
       NDelete,
       NRemove,
       NMerge,
-      NUpdate,
       NSplit,
       NInner,
       NLeaf,
       NSplitEntry,
-      NMergeEntry,
-      Unknown
+      NMergeEntry
+      //NUpdate
+      //Unknown
     };
 
     class BWNode {
@@ -209,7 +199,6 @@ namespace peloton {
       protected:
       const BWNode * next_;
 
-      //TODO slot_usage, chain_length, next, if_leaf
       BWDeltaNode(const size_type &slot_usage, const size_type &chain_length, const BWNode *next, const VersionNumber &version_number):
               BWNode(slot_usage, chain_length, next->IfLeafNode(), version_number), next_(next) { }
     };
@@ -280,7 +269,7 @@ namespace peloton {
       }
 
       BWSplitNode(const size_type &slot_usage, const size_type &chain_length, const BWNode *next, const KeyType &key, const PID &right):
-              BWDeltaNode(slot_usage, chain_length, next, next->GetVersionNumber()+1), key_(key), right_(right) { }
+              BWDeltaNode(slot_usage, chain_length, next, (VersionNumber)(next->GetVersionNumber()+1)), key_(key), right_(right) { }
       BWSplitNode(const size_type &slot_usage, const BWNode *next, const KeyType &key, const PID &right):
               BWSplitNode(slot_usage, next->GetChainLength()+1, next, key, right) { }
 
@@ -369,38 +358,6 @@ namespace peloton {
       }
     };
 
-    enum OpType {
-      OInsert,
-      ODelete,
-      OUnknown
-    };
-
-    class Operation {
-    public:
-      virtual OpType GetOpType() const = 0;
-
-    };
-
-    class InsertOperation: public Operation {
-    public:
-      OpType GetOpType() {
-        return OInsert;
-      }
-    };
-
-    class DeleteOperation: public Operation {
-    public:
-      OpType GetOpType() {
-        return ODelete;
-      }
-    };
-
-    // todo:
-
-
-
-
-
     typedef const BWNode * Address;
     class PIDTable {
       /*
@@ -426,7 +383,7 @@ namespace peloton {
       static const PID PID_NULL;
       //static constexpr PID PID_ROOT = 0;
 
-      PIDTable(): counter_(0) {
+      PIDTable(): counter_(0), free_PIDs(256) {
         first_level_table_[0] = (Address *)malloc(sizeof(Address)*second_level_slots);
       }
 
@@ -475,7 +432,7 @@ namespace peloton {
       private:
       Address *first_level_table_[first_level_slots] = {nullptr};
       CounterType counter_;
-      boost::lockfree::stack <PID> free_PIDs;
+      boost::lockfree::stack<PID> free_PIDs;
 
       PIDTable(const PIDTable &) = delete;
 
@@ -511,52 +468,7 @@ namespace peloton {
       }
     };
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    template<typename KeyType, typename ValueType, class KeyComparator, class KeyEqualityChecker, class ValueComparator, class ValueEqualityChecker, bool Duplicate>
+   template<typename KeyType, typename ValueType, class KeyComparator, class KeyEqualityChecker, class ValueComparator, class ValueEqualityChecker, bool Duplicate>
     class BWTree {
       KeyComparator comparator_;
       KeyEqualityChecker key_equality_checker_;
@@ -566,235 +478,121 @@ namespace peloton {
       PIDTable pid_table_;
     public :
       BWTree(IndexMetadata *indexMetadata): comparator_(indexMetadata), key_equality_checker_(indexMetadata) {
+        // a BWtree has at least two nodes residing at two levels.
+        // at initialization, one root node pointing to one leaf node
         const BWNode *first_leaf_node;
         if(!Duplicate)
           first_leaf_node = new BWLeafNode<KeyType, ValueType>(std::vector<KeyType>(), std::vector<ValueType>(),
                                                                PIDTable::PID_NULL, PIDTable::PID_NULL);
         else
-          first_leaf_node = new BWLeafNode<KeyType, std::vector<ValueType>>(std::vector<KeyType>(), std::vector<std::vector<ValueType>>(),
+          first_leaf_node = new BWLeafNode<KeyType, std::vector<ValueType>>(std::vector<KeyType>(),
+                                                                            std::vector<std::vector<ValueType>>(),
                                                                             PIDTable::PID_NULL, PIDTable::PID_NULL);
         first_leaf_ = pid_table_.allocate_PID(first_leaf_node);
 
         const BWNode *root_node = new BWInnerNode<KeyType>(std::vector<KeyType>(), {first_leaf_}, PIDTable::PID_NULL,
-                                                           PIDTable::PID_NULL, std::numeric_limits<VersionNumber>::min());
+                                                           PIDTable::PID_NULL,
+                                                           std::numeric_limits<VersionNumber>::min());
         root_ = pid_table_.allocate_PID(root_node);
       }
 
-      bool InsertEntry(const KeyType &key, const ValueType &value) {
+      inline bool InsertEntry(const KeyType &key, const ValueType &value) {
         std::vector<PID> path = {root_};
         std::vector<VersionNumber> version_number = {pid_table_.get(root_)->GetVersionNumber()};
         return InsertEntryUtil(key, value, path, version_number);
       }
 
-      bool DeleteEntry(const KeyType &key, const ValueType &value) {
+      inline bool DeleteEntry(const KeyType &key, const ValueType &value) {
         std::vector<PID> path = {root_};
         std::vector<VersionNumber> version_number = {pid_table_.get(root_)->GetVersionNumber()};
         return DeleteEntryUtil(key, value, path, version_number);
       }
-/*
-      void ScanKey(KeyType key, std::vector<ValueType> &ret) {
-        ScanKeyUtil(PIDTable::PID_ROOT, key, ret);
-      }
 
-      void ScanAllKeys(std::vector<ValueType> & ) {
-        PIDTable & pidTable = PIDTable::get_table();
-        const BWNode *node_ptr = pidTable.get(PIDTable::PID_ROOT);
+   private:
+     bool CheckStatus(const BWNode *node, const KeyType &key, std::vector<PID> &path,
+                      std::vector<VersionNumber> &version_number);
 
-        while (node_ptr->GetType() != NLeaf) {
-          const BWInnerNode<KeyType> *cur_ptr = static_cast<const BWInnerNode<KeyType> *>(node_ptr);
+     bool InsertEntryUtil(const KeyType &key, const ValueType &value, std::vector<PID> &path,
+                          std::vector<VersionNumber> &version_number);
 
-          PID next_pid = cur_ptr->GetPIDs()[0];
-          node_ptr = pidTable.get(next_pid);
-        }
+     bool DeleteEntryUtil(const KeyType &key, const ValueType &value, std::vector<PID> &path,
+                          std::vector<VersionNumber> &version_number);
 
-        // reach first leaf node
+     const BWNode *Split(const BWNode *node_ptr, const PID &node_pid);
 
-      }
-*/
-    private:
-      bool CheckStatus(const BWNode *node, const KeyType &key, std::vector<PID> &path, std::vector<VersionNumber> &version_number);
+     const BWNode *SplitInnerNode(const BWNode *leaf_node, const PID &node_pid);
 
-      bool InsertEntryUtil(const KeyType &key, const ValueType &value, std::vector<PID> &path, std::vector<VersionNumber> &version_number);
+     const BWNode *SplitLeafNode(const BWNode *leaf_node, const PID &node_pid);
 
-      bool DeleteEntryUtil(const KeyType &key, const ValueType &value, std::vector<PID> &path, std::vector<VersionNumber> &version_number);
-      
-      const BWNode *Split(const BWNode *node_ptr, const PID &node_pid);
+     bool InsertSplitEntry(const BWNode *top, std::vector<PID> &path, std::vector<VersionNumber> &version_number);
 
-      const BWNode *SplitInnerNode(const BWNode *leaf_node, const PID &node_pid);
+     bool ExistKey(const BWNode *node_ptr, const KeyType &key);
 
-      const BWNode *SplitLeafNode(const BWNode *leaf_node, const PID &node_pid);
+     void CreateLeafNodeView(const BWNode *node_chain, std::vector<KeyType> &keys, std::vector<ValueType> &values,
+                             PID &left, PID &right);
 
-      bool InsertSplitEntry(const BWNode *top, std::vector<PID> &path, std::vector<VersionNumber> &version_number){
-        assert(path.size()==version_number.size());
-        assert(top->GetType()==NSplit);
-        const BWSplitNode<KeyType> *split_ptr = static_cast<const BWSplitNode<KeyType> *>(top);
-        const KeyType &low_key = split_ptr->GetSplitKey();
-        const PID &right_pid = split_ptr->GetRightPID();
+     void CreateLeafNodeView(const BWNode *node_chain, std::vector<KeyType> &keys,
+                             std::vector<std::vector<ValueType>> &values, PID &left, PID &right);
 
-        if(path.size()>1) { // In this case, this is a normal second step split
-          // get the parent
-          PID pid = path[path.size()-2];
-          while(true) {
-            const BWNode *parent_node = pid_table_.get(pid);
-            if(version_number[version_number.size()-2]!=parent_node->GetVersionNumber())
-              return false;
+     void CreateInnerNodeView(const BWNode *node_chain, std::vector<KeyType> &keys, std::vector<PID> &children,
+                              PID &left, PID &right);
 
-            std::vector<KeyType> keys_view;
-            std::vector<PID> children_view;
-            PID left_view, right_view;
-            CreateInnerNodeView(parent_node, keys_view, children_view, left_view, right_view);
-            auto position = std::upper_bound(keys_view.cbegin(), keys_view.cend(), low_key, comparator_);
+     bool Consolidate(PID cur, const BWNode *node_ptr);
 
-            // then check if this split entry has been inserted by others
-            if(position!=keys_view.cbegin()&&key_equality_checker_(low_key, *(position-1)))
-              return true;
+     const BWNode *ConstructConsolidatedInnerNode(const BWNode *node_chain);
 
-            // try to insert the split entry
-            const BWNode *split_entry;
-            if(position!=keys_view.cend())
-              split_entry = new BWSplitEntryNode<KeyType>(parent_node, low_key, *position, right_pid);
-            else
-              split_entry = new BWSplitEntryNode<KeyType>(parent_node, low_key, right_pid);
-            if(pid_table_.bool_compare_and_swap(pid, parent_node, split_entry))
-              return true;
+     const BWNode *ConstructConsolidatedLeafNode(const BWNode *node_chain);
 
-            // fail, clean up, retry
-            delete split_entry;
-          }
-        }
-        else {
-          assert(path.back()==root_);
-          const BWNode *old_root = pid_table_.get(root_);
-          VersionNumber old_root_version = old_root->GetVersionNumber();
-          PID new_pid = pid_table_.allocate_PID(old_root);
-          BWNode *new_root = new BWInnerNode<KeyType>({}, {new_pid}, PIDTable::PID_NULL, PIDTable::PID_NULL, old_root_version+1);
-          if(!pid_table_.bool_compare_and_swap(root_, old_root, new_root)) {
-            delete new_root;
-            pid_table_.free_PID(new_pid);
-            return false;
-          }
-          return false;
-          /*
-          path.push_back(new_pid);
-          version_number.insert(version_number.begin(), (VersionNumber)(old_root_version+1));
-          return InsertSplitEntry(top, key, path, version_number);
-           */
-        }
-      }
+     void ConstructConsolidatedLeafNodeInternal(const BWNode *node_chain, std::vector<KeyType> &keys,
+                                                std::vector<ValueType> &values, PID &left, PID &right);
 
-       bool ExistKey(const BWNode *node_ptr, const KeyType &key);
+     void ConstructConsolidatedLeafNodeInternal(const BWNode *node_chain, std::vector<KeyType> &keys,
+                                                std::vector<std::vector<ValueType>> &values, PID &left, PID &right);
 
-       void CreateLeafNodeView(const BWNode *node_chain, std::vector<KeyType> &keys, std::vector<ValueType> &values, PID &left, PID &right);
+     void ConstructConsolidatedInnerNodeInternal(const BWNode *node_chain, std::vector<KeyType> &keys,
+                                                 std::vector<PID> &children, PID &left, PID &right);
 
-       void CreateLeafNodeView(const BWNode *node_chain, std::vector<KeyType> &keys, std::vector<std::vector<ValueType>> &values, PID &left, PID &right);
+     void ConsolidateInsertNode(const BWInsertNode<KeyType, ValueType> *node, std::vector<KeyType> &keys,
+                                std::vector<ValueType> &values);
 
-       void CreateInnerNodeView(const BWNode *node_chain, std::vector<KeyType> &keys, std::vector<PID> &children, PID &left, PID &right);
+     void ConsolidateInsertNode(const BWInsertNode<KeyType, ValueType> *node, std::vector<KeyType> &keys,
+                                std::vector<std::vector<ValueType>> &values);
 
-       bool Consolidate(PID cur, const BWNode *node_ptr);
+     void ConsolidateDeleteNode(const BWDeleteNode<KeyType, ValueType> *node, std::vector<KeyType> &keys,
+                                std::vector<ValueType> &values);
 
-       const BWNode *ConstructConsolidatedInnerNode(const BWNode *node_chain);
+     void ConsolidateDeleteNode(const BWDeleteNode<KeyType, ValueType> *node, std::vector<KeyType> &keys,
+                                std::vector<std::vector<ValueType>> &values);
 
-       const BWNode *ConstructConsolidatedLeafNode(const BWNode *node_chain);
+     void ConsolidateSplitNode(const BWSplitNode<KeyType> *node, std::vector<KeyType> &keys,
+                               std::vector<ValueType> &values,
+                               PID &right);
 
-       void ConstructConsolidatedLeafNodeInternal(const BWNode *node_chain, std::vector<KeyType> &keys,
-                                                 std::vector<ValueType> &values, PID &left, PID &right);
-       void ConstructConsolidatedLeafNodeInternal(const BWNode *node_chain, std::vector<KeyType> &keys,
-                                                 std::vector<std::vector<ValueType>> &values, PID &left, PID &right);
+     void ConsolidateSplitNode(const BWSplitNode<KeyType> *node, std::vector<KeyType> &keys,
+                               std::vector<std::vector<ValueType>> &values,
+                               PID &right);
 
-      void ConstructConsolidatedInnerNodeInternal(const BWNode *node_chain, std::vector<KeyType> &keys,
-                                                  std::vector<PID> &children, PID &left, PID &right);
+     void ConsolidateSplitNode(const BWSplitNode<KeyType> *node, std::vector<KeyType> &keys, std::vector<PID> &children,
+                               PID &right);
 
-      void ConsolidateInsertNode(const BWInsertNode<KeyType, ValueType> *node, std::vector<KeyType> &keys, std::vector<ValueType> &values);
-      void ConsolidateInsertNode(const BWInsertNode<KeyType, ValueType> *node, std::vector<KeyType> &keys, std::vector<std::vector<ValueType>> &values);
+     void ConsolidateSplitEntryNode(const BWSplitEntryNode<KeyType> *node, std::vector<KeyType> &keys,
+                                    std::vector<PID> &children);
 
-      void ConsolidateDeleteNode(const BWDeleteNode<KeyType, ValueType> *node, std::vector<KeyType> &keys, std::vector<ValueType> &values);
-      void ConsolidateDeleteNode(const BWDeleteNode<KeyType, ValueType> *node, std::vector<KeyType> &keys, std::vector<std::vector<ValueType>> &values);
+     PID GetRightPID(const BWNode *node_ptr) {
+       const BWNode *cur_ptr = node_ptr;
+       while(cur_ptr->GetType()!=NLeaf||cur_ptr->GetType()!=NInner) {
+         cur_ptr = cur_ptr->GetNext();
+       }
 
-      void ConsolidateSplitNode(const BWSplitNode<KeyType> *node, std::vector<KeyType> &keys, std::vector<ValueType> &values,
-                                PID &right);
-      void ConsolidateSplitNode(const BWSplitNode<KeyType> *node, std::vector<KeyType> &keys, std::vector<std::vector<ValueType>> &values,
-                                PID &right);
+       return cur_ptr->GetLeft();
+     }
 
-      void ConsolidateSplitNode(const BWSplitNode<KeyType> *node, std::vector<KeyType> &keys, std::vector<PID> &children,
-                                PID &right);
+     bool DeltaInsert(const PID &cur, const BWNode *node_ptr, const KeyType &key, const ValueType &value,
+                      bool need_expand);
 
-      void ConsolidateSplitEntryNode(const BWSplitEntryNode<KeyType> *node, std::vector<KeyType> &keys,
-                                     std::vector<PID> &children);
-
-      PID GetRightPID(const BWNode *node_ptr) {
-        const BWNode *cur_ptr = node_ptr;
-        while(cur_ptr->GetType()!=NLeaf||cur_ptr->GetType()!=NInner) {
-          cur_ptr = cur_ptr->GetNext();
-        }
-
-        return cur_ptr->GetLeft();
-      }
-
-      /*
-       *  In inner levels, the delta nodes should be SplitEntryNode, MergeEntryNode, SplitNode.
-       *  For now, only handle SplitEntryNode and SplitNode.
-       *
-       *  This function has handle duplicate keys
-       *
-       *  TODO: Handle MergeEntryNode.
-       */
-/*
-      void SplitInnerNodeUtil(const BWNode *node_ptr, KeyType &split_key, PID &right_pid,
-                              std::vector<KeyType> &keys,
-                              std::vector<PID> &pids);
-*/
-
-      // TODO: handle duplicate keys
-/*
-      void SplitLeafNodeUtil(const BWNode *node_ptr, KeyType &split_key, PID &right_pid,
-                             std::vector<KeyType> &keys,
-                             std::vector<ValueType> &values);
-*/
-
-
-      bool DeltaInsert(const PID &cur, const BWNode *node_ptr, const KeyType &key, const ValueType &value, bool need_expand);
-      bool DeltaDelete(const PID &cur, const BWNode *node_ptr, const KeyType &key, const ValueType &value, bool &exist_value);
-
-      // TODO: handle MergeEntryNode
-/*
-      PID GetNextPID(PID cur, KeyType key) {
-        const BWNode *cur_ptr = PIDTable::get_table().get(cur);
-
-        while(cur_ptr->GetType()!=NInner) {
-          if(cur_ptr->GetType()==NSplitEntry) {
-            const BWSplitEntryNode<KeyType> *split_entry_ptr = static_cast<const BWSplitEntryNode<KeyType> *>(cur_ptr);
-
-            if(!comparator_(key, split_entry_ptr->GetLowKey())&&comparator_(key, split_entry_ptr->GetHightKey())) {
-              return split_entry_ptr->GetNextPID();
-            }
-          }
-          else if(cur_ptr->GetType()==NMergeEntry) {
-
-          }
-          else {
-
-          }
-
-          cur_ptr = cur_ptr->GetNext();
-        }
-
-        //const BWInnerNode<KeyType> *inner_ptr = static_cast<const BWInnerNode<KeyType> *>(cur_ptr);
-        // TODO: implement scan_key;
-
-        return PIDTable::PID_ROOT;
-      }
-*/
-
-
-//      void ScanKeyUtil(PID cur, KeyType key, std::vector<ValueType> &ret);
-
-
-    };
-
-
-    ////////////////////////////////////////////////////////////////
-
-
+     bool DeltaDelete(const PID &cur, const BWNode *node_ptr, const KeyType &key, const ValueType &value,
+                      bool &exist_value);
+   }
 
   }  // End index namespace
 }  // End peloton namespace
