a5d7594be798bda24cff6b7e70d4b6c7b644269a
amaliujia <amaliujia@163.com>
2016-03-25 00:04:02 -0400

    use cuckoohash_map in a wrong way, needs modification.

--- tests/common/cuckoohash_map_test.cpp
+++ tests/common/cuckoohash_map_test.cpp
@@ -3,6 +3,7 @@
 #include <time.h>
 
 #include "harness.h"
+#include "backend/common/logger.h"
 #include "backend/common/cuckoohash_map.h"
 #include "backend/common/types.h"
 
@@ -13,18 +14,20 @@ namespace test {
 
 class CUCKOOTest : public PelotonTest {};
 
-Typedef  cuckoohash_map<std::string,
-  std::unordered_set<std::pair<size_t, oid_t>, boost::hash<std::pair<size_t, oid_t>>>> HashMapType;
+typedef std::unordered_set<std::pair<size_t, oid_t>, boost::hash<std::pair<size_t, oid_t>>> value_type;
+
+typedef cuckoohash_map<std::string, value_type *> HashMapType;
+
+
 
 TEST_F(CUCKOOTest, BasicTest) {
-  cuckoohash_map<std::string,
-                 std::unordered_set<std::pair<size_t, oid_t>, boost::hash<std::pair<size_t, oid_t>>>
-                   > cuckoo_map;
+  HashMapType cuckoo_map;
+  
   if (cuckoo_map.contains("a")) {
-    cuckoo_map.find("a").insert(std::make_pair(12,12));
+    cuckoo_map.find("a")->insert(std::make_pair(12,12));
   } else {
-    cuckoo_map.insert("a", std::unordered_set<std::pair<size_t, oid_t>, boost::hash<std::pair<size_t, oid_t>>>());
-    cuckoo_map.find("a").insert(std::make_pair(12,12));
+    cuckoo_map.insert("a", new value_type());
+    cuckoo_map.find("a")->insert(std::make_pair(12,12));
   }
   EXPECT_EQ(cuckoo_map.size(), 1);
 }
@@ -38,19 +41,29 @@ void Insert(HashMapType *cuckoo_map) {
   size_t iSecret = rand() % 1000000 + 1;
   for (int i = 0; i < 30; i++) {
     if (!cuckoo_map->contains(vec[i])) {
-      cuckoo_map->insert(vec[i], std::unordered_set<std::pair<size_t, oid_t>, boost::hash<std::pair<size_t, oid_t>>>());
+      cuckoo_map->insert(vec[i], new value_type());
     }
     size_t lv = iSecret + i;
-    oid_t lv = iSecret + i + 1;
-    cuckoo_map->find(vec[i]).insert(std::make_pair<lv, tv>);
+    oid_t tv = iSecret + i + 1;
+    
+    // value_type v;
+    cuckoo_map->find(vec[i])->insert(std::make_pair(lv, tv));
   }
 }
 
 TEST_F(CUCKOOTest, MultiThreadedTest) {
   HashMapType cuckoo_map;
+  size_t numThreads = 5;
+  LaunchParallelTest(numThreads, Insert, &cuckoo_map);
+  EXPECT_EQ(cuckoo_map.size(), 30);
+ 
+  for (int i = 0; i < 30; i++) {
+    EXPECT_EQ(cuckoo_map.contains(std::to_string(i)), true); 
+    EXPECT_EQ(cuckoo_map.find(std::to_string(i))->size(), numThreads); 
+  }
 
-  LaunchParallelTest(5, Insert, &cuckoo_map);
-  EXPECT_EQ(cockoo_map.size(), 150);
+  // const auto& pair_set = cuckoo_map.find(std::to_string(10));
+  // EXPECT_EQ(pair_set.find(std::make_pair(0, 0)) != pair_set.end(), true);
 }
 
 }
